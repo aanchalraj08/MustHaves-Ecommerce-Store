42734c6b3552f2f7120dfe080d45e428942703a8
aanchalraj <anchalraj0811@gmail.com>
2023-04-02 21:07:20 +0530

    final ✨

---
+++ src/data.js
@@ -0,0 +1,89 @@
+export const sliderItems=[
+    {
+        id:1,
+        img:"https://i.ibb.co/DG69bQ4/1.png",
+        title:"SUMMER SALE",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg:"f5fafd",
+    },
+    {
+        id:2,
+        img:"https://img.freepik.com/free-photo/good-advice-from-beautiful-woman_329181-3527.jpg?w=900&t=st=1678471436~exp=1678472036~hmac=9179336dea9c3405ea0575e51246805e816a468b8fccd6882aef57e0247806d3",
+        title:"AUTMN SALE",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg:"fcf1ed",
+    },
+    
+    {
+        id:3,
+        img:"https://i.ibb.co/cXFnLLV/3.png",
+        title:"POPULAR SALE",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg:"fbf0f4",
+    },
+];
+
+
+
+export const categories=[
+    {
+        id:1,
+        img:"https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/2148614786/settings_images/K2AL1QscQ3Sw1Hex5OWC_Untitled_design_-_2021-08-25T003605.579.png",
+        title: "SHIRT STYLE!",
+        cat: "women"
+    },
+
+    {
+        id:2,
+        img:"https://bottomlessbrunch.com/wp-content/uploads/2021/05/pexels-osmar-vasques-1868582-600x902.jpg",
+        
+        //https://honestexplorer.com/wp-content/uploads/2021/07/woman-with-hands-in-air.jpg
+        title:"LOUNGEWEAR LOVE",
+        cat: "coat"
+    },
+
+    {
+        id:3,
+        img:"https://bottomlessbrunch.com/wp-content/uploads/2021/05/pexels-dziana-hasanbekava-5480696-600x898.jpg",
+        title:"LIGHT JACKETS",
+        cat: "jeans"
+    },
+
+
+];
+
+
+export const popularProducts = [
+    {
+      id:1,
+      img:"https://img101.urbanic.com/v1/goods-pic/0f40ac0d6957486b92362cbafec3be38_w1000_q90.webp",
+    },
+    {
+      id:2,
+      img:"https://img101.urbanic.com/v1/goods-pic/8d812ff087d4409489ec6302b4b2eb8fUR_w1000_q90.webp",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://img101.urbanic.com/v1/goods-pic/1063d24d80a14dfc825312801b76303d_w1000_q90.webp",
+    },
+    {
+      id:5,
+      img:"https://img101.urbanic.com/v1/goods-pic/7ff62feb5e0e418d8b16bbf30564d50fUR_w1000_q90.webp",
+    },
+    {
+      id:6,
+      img:"https://img101.urbanic.com/v1/goods-pic/3c2a280c05b6468bb1e966bb6a4785a8_w540_h720_q70.webp",
+    },
+    {
+      id:7,
+      img:"https://img101.urbanic.com/v1/goods-pic/354de178f7694a578931766a341adafe_w540_h720_q85.webp",
+    },
+    {
+      id:8,
+      img:"https://img101.urbanic.com/v1/goods-pic/8bac62bb73304f45aa2cd1b80785c452_w1000_q90.webp",
+    },
+  ]
